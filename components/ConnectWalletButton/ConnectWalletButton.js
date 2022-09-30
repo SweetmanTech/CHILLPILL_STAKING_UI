bb4f5a629cb9a02da6c7f0062bf03a9a52591454
Sweets Sweetman <sweetmantech@gmail.com>
2022-09-30 16:01:41 -0300

    I init <ConnectWalletButton /> with ChillPill svg asset.

---
+++ components/ConnectWalletButton/ConnectWalletButton.js
@@ -0,0 +1,70 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useState } from "react";
+import ConnectSvg from "./ConnectSvg";
+
+const ConnectWalletButton = () => {
+  const [hovering, setHovering] = useState(false);
+
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <div
+                    onMouseEnter={() => {
+                      console.log("HOVERING");
+                      setHovering(true);
+                    }}
+                    onMouseLeave={() => {
+                      console.log("LEAVE HOVERING");
+
+                      setHovering(false);
+                    }}
+                    onClick={openConnectModal}
+                  >
+                    <ConnectSvg
+                      width={200}
+                      height={25}
+                      fillColor={hovering ? "#FD0101" : "#FAF400"}
+                    />
+                  </div>
+                );
+              }
+              return <ConnectButton />;
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+};
+
+export default ConnectWalletButton;
