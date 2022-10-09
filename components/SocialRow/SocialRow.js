bec4e6676d06278a67f9a724947baa73bd5247bc
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-08 23:40:51 -0500

    I add OpenSea link.

--- components/SocialRow/SocialRow.js
+++ components/SocialRow/SocialRow.js
@@ -1,11 +1,19 @@
 import { Box } from "@mui/material";
 import Discord from "../SVG/Discord";
+import OpenSea from "../SVG/OpenSea";
 import Twitter from "../SVG/Twitter";
 
 const SocialRow = () => {
   return (
-    <Box style={{ display: "flex", justifyContent: "space-between" }}>
-      <h1>Hello World</h1>
+    <Box
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "90vw",
+      }}
+    >
+      <OpenSea style={{ height: "75px" }} />
       <Box style={{ display: "flex", gap: 30 }}>
         <Twitter style={{ width: "50px" }} />
         <Discord style={{ width: "50px" }} />
