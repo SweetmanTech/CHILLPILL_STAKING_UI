9362148200f5e3fb0e672bf0b2f9c1da41f188ad
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-09 01:59:38 -0500

    I remove comments.

--- components/SVG/SteakChatSvg/SteakChatSvg.js
+++ components/SVG/SteakChatSvg/SteakChatSvg.js
@@ -15,12 +15,12 @@ const SteakChatSvg = ({ amountOfChill = "", style, chillTokenAddres }) => {
       await ethereum.request({
         method: "wallet_watchAsset",
         params: {
-          type: "ERC20", // Initially only supports ERC20, but eventually more!
+          type: "ERC20",
           options: {
-            address: tokenAddress, // The address that the token is at.
-            symbol: tokenSymbol, // A ticker symbol or shorthand, up to 5 chars.
-            decimals: tokenDecimals, // The number of decimals in the token
-            image: tokenImage, // A string url of the token logo
+            address: tokenAddress,
+            symbol: tokenSymbol,
+            decimals: tokenDecimals,
+            image: tokenImage,
           },
         },
       });
