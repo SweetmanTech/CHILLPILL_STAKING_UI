70a9238b30dd35817db3663c43561b9c317e0094
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-08 23:56:29 -0500

    I add <WhatIsChill /> component.

--- components/SocialRow/SocialRow.js
+++ components/SocialRow/SocialRow.js
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import Discord from "../SVG/Discord";
 import OpenSea from "../SVG/OpenSea";
 import Twitter from "../SVG/Twitter";
+import WhatIsChill from "../SVG/WhatIsChill";
 
 const SocialRow = () => {
   return (
@@ -17,6 +18,7 @@ const SocialRow = () => {
       <Box style={{ display: "flex", gap: 30 }}>
         <Twitter style={{ width: "50px" }} />
         <Discord style={{ width: "50px" }} />
+        <WhatIsChill style={{ height: "50px" }} />
       </Box>
     </Box>
   );
