4528d80de573ebdc793041df1087949ab2849d00
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-09 20:14:31 -0500

    I update LoginSpeechBubble SVG.

--- components/LoginPage/LoginPage.js
+++ components/LoginPage/LoginPage.js
@@ -1,21 +1,7 @@
-import ConnectWalletButton from "../ConnectWalletButton";
 import LoginSpeechBubble from "../LoginSpeechBubble";
 
 const LoginPage = () => {
-  return (
-    <>
-      <LoginSpeechBubble style={{ position: "relative", zIndex: 0 }} />
-      <ConnectWalletButton
-        style={{
-          "enable-background": "new 0 0 2048 2048;",
-          zIndex: 1000,
-          position: "absolute",
-          top: "50%",
-          right: "20%",
-        }}
-      />
-    </>
-  );
+  return <LoginSpeechBubble style={{}} />;
 };
 
 export default LoginPage;
