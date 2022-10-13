1d000741017ea92507e3c75c1086be0c7c90ec71
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-13 09:24:31 -0500

    I update social Row with ChillRxLogo.

--- components/SocialRow/SocialRow.js
+++ components/SocialRow/SocialRow.js
@@ -1,23 +1,11 @@
 import { Box, useMediaQuery } from "@mui/material";
 import Discord from "../SVG/Discord";
-import OpenSea from "../SVG/OpenSea";
 import Twitter from "../SVG/Twitter";
-import WhatIsChill from "../SVG/WhatIsChill";
-import StakeAllButton from "../SVG/StakeAllButton";
-import ClaimButton from "../SVG/ClaimButton";
 import { useSigner } from "wagmi";
+import ChillRxLogo from "../SVG/ChillRxLogo";
 
-const SocialRow = ({
-  nftContract,
-  stakingContract,
-  stakedTokens,
-  setPendingTxStep,
-  unclaimedChill,
-  unstakedTokens,
-  load,
-}) => {
+const SocialRow = ({}) => {
   const isMobile = useMediaQuery("(max-width:600px)");
-  const { data: signer } = useSigner();
 
   return (
     <Box
@@ -25,41 +13,11 @@ const SocialRow = ({
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
-        width: "100vw",
+        width: "90vw",
         flexWrap: "wrap",
       }}
     >
-      <ClaimButton
-        style={{ height: isMobile ? "25px" : "50px" }}
-        stakingContract={stakingContract}
-        stakedTokenIds={stakedTokens}
-        setPendingTxStep={setPendingTxStep}
-        onSuccess={() => {
-          load(signer);
-          setPendingTxStep(0);
-        }}
-        unclaimedChill={unclaimedChill}
-      />
-      <StakeAllButton
-        stakingContract={stakingContract}
-        nftContract={nftContract}
-        tokensToStake={unstakedTokens}
-        onSuccess={() => {
-          load(signer);
-          setPendingTxStep(0);
-        }}
-        style={{
-          height: isMobile ? "25px" : "50px",
-          minHeight: isMobile ? "25px" : "50px",
-        }}
-        setPendingTxStep={setPendingTxStep}
-      />
-      {!isMobile && <OpenSea style={{ height: isMobile ? "35px" : "75px" }} />}
-      <Box style={{ display: "flex", gap: 30 }}>
-        <Twitter style={{ width: isMobile ? "25px" : "50px" }} />
-        <Discord style={{ width: isMobile ? "25px" : "50px" }} />
-        <WhatIsChill style={{ height: isMobile ? "25px" : "50px" }} />
-      </Box>
+      <ChillRxLogo style={{ width: "100px" }} />
     </Box>
   );
 };
