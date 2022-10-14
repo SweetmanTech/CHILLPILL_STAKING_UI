9389ea72a789d9323a9310b9b649010e1fc57ab7
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-14 01:45:01 -0500

    I init new util getTruncatedWallet.

--- components/SocialRow/SocialRow.js
+++ components/SocialRow/SocialRow.js
@@ -1,23 +1,18 @@
 import { Box } from "@mui/material";
 import { useAccount } from "wagmi";
+import getTruncatedWallet from "../../lib/getTruncatedWallet";
 import ChillRxLogo from "../SVG/ChillRxLogo";
 import WalletAddressBox from "../SVG/WalletAddressBox";
 
 const SocialRow = ({ style }) => {
   const { address: account } = useAccount();
 
-  const truncatedAccount =
-    account &&
-    `${account.substring(0, 4)}...${account.substring(account.length - 3)}`;
-
-  console.log("ACCOUNT", account);
-
   return (
     <Box style={style}>
       <ChillRxLogo style={{ width: "100px" }} />
       {account && (
         <WalletAddressBox
-          address={truncatedAccount}
+          address={getTruncatedWallet(account)}
           style={{ width: "200px" }}
         />
       )}
