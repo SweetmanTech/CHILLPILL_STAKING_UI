715a672f1e5ff488c8cec6ccca15d44fe1cc977d
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-14 01:42:34 -0500

    I add WalletConnected to Socialrow (should probs rename this to TopRow).

--- components/SocialRow/SocialRow.js
+++ components/SocialRow/SocialRow.js
@@ -1,13 +1,26 @@
-import { Box, useMediaQuery } from "@mui/material";
-import Discord from "../SVG/Discord";
-import Twitter from "../SVG/Twitter";
-import { useSigner } from "wagmi";
+import { Box } from "@mui/material";
+import { useAccount } from "wagmi";
 import ChillRxLogo from "../SVG/ChillRxLogo";
+import WalletAddressBox from "../SVG/WalletAddressBox";
 
 const SocialRow = ({ style }) => {
+  const { address: account } = useAccount();
+
+  const truncatedAccount =
+    account &&
+    `${account.substring(0, 4)}...${account.substring(account.length - 3)}`;
+
+  console.log("ACCOUNT", account);
+
   return (
     <Box style={style}>
       <ChillRxLogo style={{ width: "100px" }} />
+      {account && (
+        <WalletAddressBox
+          address={truncatedAccount}
+          style={{ width: "200px" }}
+        />
+      )}
     </Box>
   );
 };
