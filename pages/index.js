3fbb65080decc1052ef7da98a029b5d881f80a62
Sweets Sweetman <sweetmantech@gmail.com>
2022-10-14 20:43:51 -0500

    I add fallback for rate-limited OpenSea API.

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useAccount } from "wagmi";
 import styles from "../styles/Home.module.css";
 import Head from "components/Head";
-import SocialRow from "components/SocialRow";
+import TopRow from "components/TopRow";
 import StakingPage from "components/StakingPage";
 import LoginSpeechBubble from "components/SVG/LoginSpeechBubble";
 import StakingData from "components/SVG/StakingData";
@@ -17,7 +17,7 @@ const Home = ({ totalStakedPills, floorPrice }) => {
     <Box sx={{ backgroundColor: "#111827" }} className={styles.container}>
       <Head />
 
-      <SocialRow
+      <TopRow
         style={{
           paddingTop: "2.5%",
           display: "flex",
@@ -66,7 +66,7 @@ const Home = ({ totalStakedPills, floorPrice }) => {
 export async function getServerSideProps(context) {
   const res = await fetch("https://api.opensea.io/collection/chillrx");
   const openSeaData = await res.json();
-  const floorPrice = openSeaData.collection.stats.floor_price;
+  const floorPrice = openSeaData?.collection?.stats?.floor_price || 0;
   const chainId = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID);
   const provider = getDefaultProvider(chainId);
   const readOnlyContracts = await getStakingContracts(provider);
